3468e9150dfc84226cd99a94d9ca1aba4b068fcd
Abinaya <abinayamonica1215@gmail.com>
2026-08-30 10:32:51 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'node:fs'
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        const distDir = path.resolve('dist')
+        const indexHtml = path.join(distDir, 'index.html')
+        const fallbackHtml = path.join(distDir, '404.html')
+        if (fs.existsSync(indexHtml)) {
+          fs.copyFileSync(indexHtml, fallbackHtml)
+        }
+      }
+    }
+  ],
   base: '/Elevora/',
 })
+
+
